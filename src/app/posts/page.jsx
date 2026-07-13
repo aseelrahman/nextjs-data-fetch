ad817014a1e2724405708619809ed2c5b529f505
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-07-13 10:23:49 -0400

    fetching data in server components

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,11 +1,35 @@
+// const getPosts = async () => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     return res.json();
+// }
 
+const getPosts = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    if(!res.ok){
+        throw new Error('Failed to fetch posts')
+    }
+    return res.json();
+}
 
-const PostsPage = () => {
-    return (
-        <div>
-            <h2>Posts are coming soon!</h2>
-        </div>
-    );
+// const getPosts = async () => {
+//   try {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     return res.json();
+//   } catch (err) {
+//     throw new Error("Failed to fetch posts");
+//   }
+// };
+
+const PostsPage = async () => {
+  // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  // const posts = await res.json();
+
+  const posts = await getPosts();
+  return (
+    <div>
+      <h2>Posts are coming soon: {posts.length}</h2>
+    </div>
+  );
 };
 
 export default PostsPage;
